064acfc1d5871e03cd48866a0d3095b54bf1b883
Rodney Lewis <rodneylewis95@gmail.com>
2025-03-08 12:50:45 -0500

    Update AlgaeEndEffector.java
    - Quick cleanup

--- src/main/java/frc/robot/subsystem/AlgaeEndEffector.java
+++ src/main/java/frc/robot/subsystem/AlgaeEndEffector.java
@@ -8,41 +8,39 @@ import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.swerve.SwerveDrivetrain;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class AlgaeEndEffector extends SubsystemBase {
-  /** Creates a new AlgaeEndEffector. */
-  public AlgaeEndEffector() {
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
-  private TalonFX wrisTalonFX = new TalonFX(22);
-  private TalonFX intakeTalonFX = new TalonFX(23);
-  private DigitalInput IRSensor = new DigitalInput(5);
-  private CANcoder absCaNcoder = new CANcoder(24);
-
   public enum Position {
-    Home(0),
-    Reef(0),
-    Floor(0),
-    Barge(0),
-    Topofcoral(0);
+    HOME(0),
+    REEF(0),
+    FLOOR(0),
+    BARGE(0),
+    TOP_OF_CORAL(0);
 
     public final double rotations;
 
     Position(double rotations) {
       this.rotations = rotations;
     }
+  }
 
-    private PositionVoltage wristPositionControl = new PositionVoltage(0).withSlot(0);
-    private VelocityVoltage intakevolocitycontrol = new VelocityVoltage(0).withSlot(0);
-    private PositionVoltage intakePositionControl = new PositionVoltage(0).withSlot(0);
+  private TalonFX wristTalonFX = new TalonFX(22);
+  private CANcoder absoluteEncoder = new CANcoder(24);
+  private PositionVoltage wristPositionControl = new PositionVoltage(0).withSlot(0);
+
+  private TalonFX intakeTalonFX = new TalonFX(23);
+  private DigitalInput algaeSensor = new DigitalInput(5);
+  private VelocityVoltage intakevolocitycontrol = new VelocityVoltage(0).withSlot(0);
+  private PositionVoltage intakePositionControl = new PositionVoltage(0).withSlot(0);
+
+  public AlgaeEndEffector() {
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
   }
 }
