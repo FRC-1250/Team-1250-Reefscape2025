87aba29c4622540fe13412ab525d48fb8f27a045
Rodney Lewis <rodneylewis95@gmail.com>
2025-08-10 14:09:41 -0400

    Remove teleop automation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,14 +7,12 @@ package frc.robot;
 import static edu.wpi.first.units.Units.*;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
-import com.ctre.phoenix6.swerve.SwerveRequest.FieldCentricFacingAngle;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.filter.SlewRateLimiter;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -23,7 +21,6 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystem.CommandSwerveDrivetrain;
 import frc.robot.subsystem.DeepClimber;
@@ -47,11 +44,6 @@ public class RobotContainer {
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
             .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1)
             .withDriveRequestType(DriveRequestType.Velocity);
-
-    private final SwerveRequest.FieldCentricFacingAngle driveFacingAngle = new FieldCentricFacingAngle()
-            .withDeadband(MaxSpeed * 0.1)
-            .withDriveRequestType(DriveRequestType.Velocity)
-            .withHeadingPID(5, 0, 0);
 
     private final Telemetry logger = new Telemetry();
     private final SendableChooser<Command> autoChooser = new SendableChooser<>();
@@ -87,9 +79,6 @@ public class RobotContainer {
 
     private final SlewRateLimiter xLimiter = new SlewRateLimiter(14, -18, 0);
     private final SlewRateLimiter yLimiter = new SlewRateLimiter(14, -18, 0);
-
-    private final Trigger isBlue = new Trigger(
-            () -> Double.compare(drivetrain.getOperatorForwardDirection().getDegrees(), 0) == 0);
 
     public RobotContainer() {
         configureBindings();
@@ -129,16 +118,6 @@ public class RobotContainer {
                         .withRotationalRate(-primaryDriverJoystick.getRightX() * MaxAngularRate))
                         .withName("Field centric swerve"));
 
-        primaryDriverJoystick.x().whileTrue(drivetrain.applyRequest(
-                () -> driveFacingAngle
-                        .withVelocityX(yLimiter.calculate(-primaryDriverJoystick.getLeftY() * MaxSpeed))
-                        .withVelocityY(xLimiter.calculate(-primaryDriverJoystick.getLeftX() * MaxSpeed))
-                        .withTargetDirection(Rotation2d.fromDegrees(
-                                drivetrain.getOperatorForwardDirection().getDegrees())))
-                .withName("Forward and strafe"));
-
-        primaryDriverJoystick.a().and(isBlue).whileTrue(controlFactory.cmdFollowPathToBlueReefTag());
-        primaryDriverJoystick.a().and(isBlue.negate()).whileTrue(controlFactory.cmdFollowPathToRedReefTag());
         primaryDriverJoystick.start()
                 .onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()).withName("Reseed drive"));
 
